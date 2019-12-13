9565ff01d98d9ce48cd6c04b6948488d69f0f537
olebogeng <seribeolebogeng@gmail.com>
2019-12-13 10:11:25 +0200

    test names

--- test_calculator.py
+++ test_calculator.py
@@ -1,22 +1,22 @@
 from calculator import add,multiply
 #Test addition
-def test_function():
+def test_add_zeros_function():
     assert add(0,0) == 0
 
-def test_function():
+def test_add_negatives_function():
     assert add(-1,-1) == -2 
 
-def test_function():
+def test_add_positives_function():
     assert add(4,5)  == 9  
 
-def test_function():
+def test_add_multiples_function():
     assert add(1,2,3,4) == 10  
 
 #Test multiply
 
-def test_function():
+def test_multiply_positives_function():
     assert multiply(1,2) == 2
 
-def test_function():
+def test_multiply_many_function():
     assert multiply(1,2,3,4) == 24   
 
